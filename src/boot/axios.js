956d8337ff6f854f4563c0f4fb674c8ff85dc91b
Yuriy Bevov <jury19y@mail.ru>
2021-10-17 18:15:07 +0300

    ip

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import axios from 'axios'
 import qs from 'qs'
 
+axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.get['Content-Type'] ='application/json;charset=utf-8';
+axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+
+console.log(axios.defaults.headers)
+
 const axiosInstance = axios.create({
   paramsSerializer: params => {
     return qs.stringify(params, { arrayFormat: 'repeat' })
   },
-  baseURL: 'http://89.223.66.137'
+
+  baseURL: 'http://5.23.54.158'
+  //baseURL: 'http://127.0.0.1:3000'
 })
 
 const setErrorInterceptor = (errorFunction) => {
